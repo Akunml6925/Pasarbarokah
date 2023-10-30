3cdd2729bf3d2009f7825b3310f0bcfd735e110c
akunml6925@gmail.com <akunml6925@gmail.com>
2023-10-30 10:20:34 +0700

    merubah lagi

--- konfigurasi-firebase.js
+++ konfigurasi-firebase.js
@@ -5,5 +5,5 @@ apiKey: "AIzaSyA-wvBGzlYI9NHjVZBq7wbUHtEWrN3AFI8",
   storageBucket: "pasarbarokah-56d6c.appspot.com",
   messagingSenderId: "316348641371",
   appId: "1:316348641371:web:5ad38a561e7d73744acf7e",
-  measurementId: 
+  measurementId:"G-NKKFY4X1ZC" 
 };
