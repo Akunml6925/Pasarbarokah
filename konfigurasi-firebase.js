c9bfd78bebf35539efa69e226d80dccc9d53c88b
akunml6925@gmail.com <akunml6925@gmail.com>
2023-10-30 11:13:55 +0700

    test

--- konfigurasi-firebase.js
+++ konfigurasi-firebase.js
@@ -1,9 +1,9 @@
 export const konfigurasiFirebase = {
-apiKey: "AIzaSyA-wvBGzlYI9NHjVZBq7wbUHtEWrN3AFI8",
+  apiKey: "AIzaSyA-wvBGzlYI9NHjVZBq7wbUHtEWrN3AFI8",
   authDomain: "pasarbarokah-56d6c.firebaseapp.com",
   projectId: "pasarbarokah-56d6c",
   storageBucket: "pasarbarokah-56d6c.appspot.com",
   messagingSenderId: "316348641371",
   appId: "1:316348641371:web:5ad38a561e7d73744acf7e",
-  measurementId:"G-NKKFY4X1ZC" 
+  measurementId: "G-NKKFY4X1ZC"
 };
